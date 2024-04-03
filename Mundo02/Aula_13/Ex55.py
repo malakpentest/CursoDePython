0a25f659029a564571103137b27e01162d1c5e62
Fernando Lage Novaes <108960402+malakpentest@users.noreply.github.com>
2024-04-03 09:17:14 -0300

    Create Ex55.py

---
+++ Mundo02/Aula_13/Ex55.py
@@ -0,0 +1,15 @@
+#Crie um Script em Python que leia o peso de cinco pessoas. No final, mostre qual foi o maior e o menor peso lidos.
+
+maior = 0
+menor = 0
+for c in range(1, 6):
+    peso = float(input('Digite o peso da {}ª pessoa:'.format(c)))
+    if c == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido e {}Kg \nO menor peso lido e {}Kg'.format(maior, menor))
