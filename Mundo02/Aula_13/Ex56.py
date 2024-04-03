8a3000b4c2d979cbb0da0edd72e26c353a679df9
Fernando Lage Novaes <108960402+malakpentest@users.noreply.github.com>
2024-04-03 09:20:15 -0300

    Update Ex56.py

--- Mundo02/Aula_13/Ex56.py
+++ Mundo02/Aula_13/Ex56.py
@@ -31,3 +31,31 @@ mediaidade = somaidade / 4
 print('A média de idade do grupo éa de {.:1f} anos'.format(mediaidade))
 print('O homem mais velhor tem {} anos e se chama {}.'.format(maioridadehomem, nomevelhor))
 print('Ao total de mulheres com menos de 20 anos {}.'.format(totmulher20))
+
+
+#OU
+
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelhor = ''
+totmulher20 = 0
+for p in range(1, 5):
+    print('----- {}ª PESSOA -----'.format(p))
+    n = str(input('Nome: ')).strip()
+    i = int(input('Idade: '))
+    s = str(input('Sexo [M/F]: ')).strip()
+    somaidade += i
+    if p == 1 and s in 'Mm':
+        maioridadehomem = i
+        nomevelhor = n
+    if s in 'Mm' and i > maioridadehomem:
+        maioridadehomem = i
+        nomevelhor = n
+    if s in 'Ff' and i < 20:
+        totmulher20 += 1
+
+mediaidade = somaidade / 4
+print('A média de idade do grupo éa de {.:1f} anos'.format(mediaidade))
+print('O homem mais velhor tem {} anos e se chama {}.'.format(maioridadehomem, nomevelhor))
+print('Ao total de mulheres com menos de 20 anos {}.'.format(totmulher20))
