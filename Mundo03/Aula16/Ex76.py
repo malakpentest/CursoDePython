00a6a4ebd3d75b145500f61253f19474ac7c2235
Fernando Lage Novaes <108960402+malakpentest@users.noreply.github.com>
2024-07-17 12:35:23 -0300

    Update Ex76.py

--- Mundo03/Aula16/Ex76.py
+++ Mundo03/Aula16/Ex76.py
@@ -15,3 +15,16 @@ print('{}{}R$    {:.2f}'.format(listagem[16],'.'*44,listagem[17]))
 print('{}{}R$  {:.2f}'.format(listagem[18],'.'*43,listagem[19]))
 print('{}{}R$    {:.2f}'.format(listagem[20],'.'*45,listagem[21]))
 print('=='*30)
+
+#OU
+
+listagem = ('Caneta',1.90,'Lápis',1.00,'Borracha',0.50,'Apontador', 0.90,'Caderno',4.90,'Agenda',19.90,'Revista',15.49,'Compasso',3.99,'Estojo',4.99,'Mochila',149.90,'Regua', 6.90 )
+print('-'*40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-'*40)
+for pos in range(0,len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}',end='')
+    else:
+        print(f'R${listagem[pos]:>7.2f}')
+print('-'*40)
